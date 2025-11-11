003a92b426d882468c2392a0a3f1707eb9ac5c1b
u481437 <hjouhar@pca.ma>
2025-11-11 11:33:53 +0100

    chore: ignore IntelliJ project files

---
+++ services/common-security/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.ledgerx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
